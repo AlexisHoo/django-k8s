ef86fa346e031a665d4fdc24c2b032d828a1c668
AlexisHoo <148786652+AlexisHoo@users.noreply.github.com>
2024-05-05 00:45:39 +0200

    end of docker compose part1

--- web/django_k8s/settings.py
+++ web/django_k8s/settings.py
@@ -82,22 +82,22 @@ DATABASES = {
 }
 
 
-DB_USERNAME = os.environ.get("POSTGRES_USERNAME")
+DB_USERNAME = os.environ.get("POSTGRES_USER")
 DB_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
 DB_DATABASE = os.environ.get("POSTGRES_DB")
 DB_HOST = os.environ.get("POSTGRES_HOST")
-DB_PORT = os.environ.get("POSTGRES_HOST")
+DB_PORT = os.environ.get("POSTGRES_PORT")
 DB_IS_AVAILABLE = all([
     DB_USERNAME,
     DB_PASSWORD,
     DB_DATABASE,
     DB_HOST,
-    DB_PORT
+    DB_PORT,
 ])
 
-POSTGRES_READY = str(os.environ.get("POSTGRES_READY")) == "1"
+# POSTGRES_READY = str(os.environ.get("POSTGRES_READY")) == "1"
 
-if DB_IS_AVAILABLE and POSTGRES_READY:
+if DB_IS_AVAILABLE: # and POSTGRES_READY
     DATABASES = {
         "default":{
             "ENGINE":"django.db.backends.postgresql",
--- web/migrate.sh
+++ web/migrate.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
 SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL:-"hoorewegealexis@gmail.com"}
 cd /app/
 
 /opt/venv_k8s/bin/python manage.py migrate --noinput
-/opt/venv_k8s/bin/python manage.py createsuperuser $SUPERUSER_EMAIL --noinput || true
+/opt/venv_k8s/bin/python manage.py createsuperuser --email $SUPERUSER_EMAIL --noinput || true
