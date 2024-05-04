9480f7140594b7e3b369406530ee07d023a535ab
AlexisHoo <148786652+AlexisHoo@users.noreply.github.com>
2024-05-04 16:27:03 +0200

    SETUP DATABASE settings

--- web/manage.py
+++ web/manage.py
@@ -2,13 +2,13 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+import dotenv
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_k8s.settings')
     try:
-        from django.core.management import execute_from_command_line
+        from django.core.management import execute_from_command_line # type: ignore
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
@@ -19,4 +19,5 @@ def main():
 
 
 if __name__ == '__main__':
+    dotenv.read_dotenv()
     main()
